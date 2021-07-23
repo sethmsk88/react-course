9174900c0b1a56f79168f98a98f5724dc9bc0b82
Seth Kerr <Michael.Kerr@talgov.com>
2021-07-23 15:49:02 -0400

    Event handling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,23 @@
 function Modal(props) {
-    return <div className="modal">
-        <p>Are you sure?</p>
-        <button className="btn btn--alt">Cancel</button>
-        <button className="btn">Confirm</button>
+  function cancelHandler() {
+      props.onCancel();
+  }
+
+  function confirmHandler() {
+      props.onConfirm();
+  }
+
+  return (
+    <div className="modal">
+      <p>Are you sure?</p>
+      <button className="btn btn--alt" onClick={cancelHandler}>
+        Cancel
+      </button>
+      <button className="btn" onClick={confirmHandler}>
+        Confirm
+      </button>
     </div>
+  );
 }
 
 export default Modal;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,18 +1,31 @@
+import { useState } from "react";
+import Backdrop from "./Backdrop";
+import Modal from "./Modal";
+
 function Todo(props) {
-    function deleteHandler() {
-        console.log("Click!");
-        console.log(props.text);
-    }
+  const [modalIsOpen, setModalIsOpen] = useState(false); // this is a React hook
 
-    return (
-        <div className="card">
-            <h2>{ props.text }</h2>
-            <div className="actions">
-                <button className="btn" onClick={deleteHandler}>Delete</button>
-            </div>
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
 
-        </div>
-    )
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
+      </div>
+      {/* modalIsOpen ? <Modal /> : null */}
+      { modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} /> /* another way of writing the same thing as the line above */ }
+      { modalIsOpen && <Backdrop onClick={closeModalHandler} /> }
+    </div>
+  );
 }
 
 export default Todo;
