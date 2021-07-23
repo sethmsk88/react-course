ea8ae2ac04fc9ac25c4e7193e2a5d54f57a909dc
Seth Kerr <Michael.Kerr@talgov.com>
2021-07-23 15:05:33 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Backdrop from './components/Backdrop';
+import Modal from './components/Modal';
+import Todo from './components/Todo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>My Tools</h1>
+            <Todo text='Learn React' />
+            <Todo text='Do Laundry' />
+            <Todo text='Coding challenges' />
+            <Modal/>
+            <Backdrop />
+        </div>
+    );
 }
 
 export default App;
